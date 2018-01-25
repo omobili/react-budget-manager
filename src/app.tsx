cb880f70ebc0261219c37228adee50165ad3337e
Olivier Mobili <olivier.mobili@rtlnet.fr>
2018-01-25 16:56:19 +0100

    remove Hello.tsx test component and start real app.tsx file

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,39 @@ import './styles/app.scss';
 import * as React from "react";
 import * as ReactDom from "react-dom";
 
-import { Hello } from "./ts/components/Hello";
+interface budgetData {
+    month: string;
+}
+
+
+class App extends React.Component {
+    data: Array<budgetData>;
+
+    constructor() {
+        super({});
+
+        this.data = [{
+            month: '201801'
+        }];
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h1 className="col-12">React Budget Manager !</h1>
+                    <div className="row">
+                        <div className="col-12">
+                            {this.data[0].month}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 ReactDom.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <App/>,
     document.getElementById("app")
 );
